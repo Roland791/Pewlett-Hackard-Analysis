eb2feb588e38178988d590ea60cb79cf53f5a3f1
Roland791 <84249213+Roland791@users.noreply.github.com>
2021-07-30 01:34:16 -0400

    Add files via upload

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,74 @@
+-- Deliverable 1 - The Number of Retiring Employees by Title
+--
+--Create a Table for all employees and their titles who started between 1952 and 1955
+SELECT e.emp_no, 
+	   e.first_name,
+	   e.last_name,
+   	   t.title,
+	   t.from_date,
+	   t.to_date
+Into retirement_titles
+FROM employees as e
+join titles as t
+On (e.emp_no = t.emp_no)
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+Order By emp_no;
+
+Select * From retirement_titles
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title,
+from_date,
+to_date
+INTO retirement_titles_unique
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+Select * From retirement_titles_unique
+
+--Retrieve the number of employees by their most recent job title who are about to retire.
+
+Select Count(Title),Title 
+Into retiring_titles
+from retirement_titles_unique
+Group by Title
+Order by count Desc
+
+Select * From retiring_titles
+
+--Deliverable 2 - The Employees Eligible for the Mentorship Program
+--
+--Write a query to create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program.
+
+SELECT e.emp_no, 
+	   e.first_name, 
+	   e.last_name, 
+	   e.birth_date,
+	   de.from_date,
+	   de.to_date,
+	   t.title
+INTO emp_temp
+From Employees as e
+join Dept_emp as de
+on (e.emp_no = de.emp_no)
+join titles as t
+on (e.emp_no = t.emp_no);
+
+SELECT DISTINCT ON (emp_no) emp_no, 
+	first_name, 
+	last_name, 
+	birth_date,
+	from_date,
+	to_date,
+	title
+Into mentorship_availability	
+From emp_temp
+WHERE (to_date = '9999-01-01')
+AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+--Group by emp_no
+Order by emp_no;
+
+Select * from mentorship_availability
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Pewlett-Hackard-Analysis
+
+Deliverable 3: Employee database analysis
+
+
+
+##Overview
+As an increasing number of Pewlett-Hackard employees are reaching retirement age, there is concern that valuable institutional knowledge may be lost and key roles may become vacant 
+if we don't act premptively to preserve the integrity of knowledge and prepare to fulfill employment gaps. This analysis is designed to determine the number of retiring 
+employees per title, and identify employees who are eligible to participate in a mentorship program. 
+
+##Results
+Provide a bulleted list with four major points from the two analysis deliverables. Use images as support where needed.
+
+##Summary
+Provide high-level responses to the following questions, then provide two additional queries or tables that may provide more insight into the upcoming "silver tsunami."
+
+1) How many roles will need to be filled as the "silver tsunami" begins to make an impact?
+2) Are there enough qualified, retirement-ready employees in the departments to mentor the next generation of Pewlett Hackard employees?
+
+Query 1
+Query 2
